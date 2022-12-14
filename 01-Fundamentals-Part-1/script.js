956704f80bcc926034ac87eb077571b932407c2d
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-12-14 16:06:29 +0300

    js fundamentals

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,119 @@
+/*
+let js = "amazing";
+console.log(js);
+console.log("toprak");
+console.log(23);
+let firtsName = "Omer";
+console.log(firtsName);
+
+let PI = 3.122344;
+console.log(PI);
+*/
+
+/*
+let jsIsFun = true;
+console.log(jsIsFun);
+console.log(typeof jsIsFun);
+console.log(typeof 23);
+console.log(typeof "toprak");
+jsIsFun = "YES!";
+console.log(typeof jsIsFun);
+let year;
+console.log(year);
+year = 2021;
+console.log(typeof year);
+*/
+
+/*
+let age = 30;
+age = 31;
+const birthYear = 1991;
+// birthYear = 1992;
+var job = "pogrammer";
+job = "teacher";
+*/
+
+/*
+//Macth operators
+const now = 2021;
+const ageJones = now - 1991;
+const ageSara = now - 2018;
+console.log(ageJones, ageSara);
+console.log(ageJones * 2, ageSara / 10, 2 ** 3);
+
+const firstName = "omer";
+const lastName = "toprak";
+console.log(firstName + " " + lastName);
+
+//assigment operators
+let x = 10 + 5;
+x += 5;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+console.log(ageJones > ageSara);
+*/
+/*Stirng and template
+const firstName = "toprak";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2033;
+const toprak =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(toprak);
+
+const newToprak = `I'm ${firstName}, a ${year - birthYear} year old.`;
+
+console.log(newToprak);
+
+console.log(`just a regular strin..`);
+
+console.log(
+  "string with \n\
+multiple \n\
+lines."
+);
+
+console.log(`String 
+multiple
+lines`);
+*/
+
+/*
+const age = 15;
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+
+  console.log(`Sarah is too young. Wait another
+  ${yearsLeft} years 🚗`);
+}
+*/
+
+// 5 falsy values 0, '', undefined, null, NaN
+/*console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("toprak"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+*/
+
+// == and ===
+/* 
+const age = "19";
+if (age === 19) console.log(`You just became an adult (strict)`);
+
+if (age == 19) console.log(`You just became an adult (loose)`);
+
+const favourite = Number(prompt("What is your favourite number?"));
+if (favourite == 23) {
+  console.log("Cool 23 is an amazing number!");
+} else if (favourite === 9) {
+  console.log(" 9 is an cool number!");
+} else {
+  console.log("number is not 23 or 7");
+}
+*/
